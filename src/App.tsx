04b3139164b9c0e3c0578bfbfeb7bacf5bf4e4b3
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-10 17:05:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [messages, setMessages] = useState([
+    { message: 'Hello, Viktor', id: 'hsdafgds', user: { id: 'sdfdsf', name: 'Dimych' } },
+    { message: 'Hello, Dimych', id: 'asdfsdfs', user: { id: 'asdfdg', name: 'Viktor' } },
+  ])
+
+  return (
+    <>
+      <div
+        style={{
+          border: '1px solid black',
+          padding: '10px',
+          width: '300px',
+          height: '300px',
+          overflowY: 'scroll',
+        }}
+      >
+        {messages.map((m, i) => (
+          <div key={i}>
+            <b>{m.user.name}:</b> {m.message}
+            <hr />
+          </div>
+        ))}
+      </div>
+
+      <textarea></textarea>
+      <button>Send</button>
+    </>
+  )
+}
+
+export default App
