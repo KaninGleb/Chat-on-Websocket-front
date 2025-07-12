6fec14d1cb6bf7daeb1bd0619d063a53190a3b43
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-12 20:34:25 +0300

    feat: handle user-typing event

--- src/api.ts
+++ src/api.ts
@@ -5,9 +5,14 @@ export const api = {
   createConnection() {
     this.socket = io('https://chat-on-websocket-back.onrender.com')
   },
-  subscribe(initMessagesHandler: (messages: any) => void, newMessageSentHandler: (newMessage: any) => void) {
+  subscribe(
+    initMessagesHandler: (messages: any) => void,
+    newMessageSentHandler: (newMessage: any) => void,
+    userTypingHandler: (user: any) => void,
+  ) {
     this.socket?.on('init-messages-published', initMessagesHandler)
     this.socket?.on('new-message-sent', newMessageSentHandler)
+    this.socket?.on('user-typing', userTypingHandler)
   },
   destroyConnection() {
     this.socket?.disconnect()
@@ -18,5 +23,8 @@ export const api = {
   },
   sendMessage(message: string) {
     this.socket?.emit('client-message-sent', message)
+  },
+  typeMessage() {
+    this.socket?.emit('client-typed')
   },
 }
