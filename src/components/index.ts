0e30d0eb081be333ab1ef398c6d367067b4e03ef
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-13 01:56:33 +0300

    feat: add ServerStatus and ServerStatusType to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { TypingUsersShowcase } from './TypingUsersShowcase/TypingUsersShowcase'
+
+export { ServerStatus } from './ServerStatus/ServerStatus'
+export type { ServerStatusType } from './ServerStatus/ServerStatus'
