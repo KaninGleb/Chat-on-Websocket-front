7f4bbf6932b52aafff3c1ef1d9a8cadd2d6f6cf8
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-15 19:44:41 +0300

    feat(ServerStatus): handle users count showing

--- src/common/components/ServerStatus/ServerStatus.tsx
+++ src/common/components/ServerStatus/ServerStatus.tsx
@@ -1,10 +1,11 @@
 import { useSelector } from 'react-redux'
-import { selectConnectionStatus, selectReadyToSendMessagesStatus } from '../../../app/chat-slice.ts'
+import { selectConnectionStatus, selectReadyToSendMessagesStatus, selectUsersCount } from '../../../app/chat-slice.ts'
 import s from './ServerStatus.module.css'
 
 export const ServerStatus = () => {
   const connectionStatus = useSelector(selectConnectionStatus)
   const readyToSendMessages = useSelector(selectReadyToSendMessagesStatus)
+  const usersCount = useSelector(selectUsersCount)
 
   const statusMap = {
     online: { color: '#4caf50', text: 'Live' },
@@ -26,11 +27,14 @@ export const ServerStatus = () => {
 
   return (
     <div className={s.container} style={{ color }}>
-      <span
-        className={`${s.content} ${statusKey === 'connecting' ? s.pulsing : ''}`}
-        style={{ backgroundColor: color }}
-      />
-      <span>{text}</span>
+      {statusKey === 'online' && <span className={s.usersCount}>({usersCount} online)</span>}
+      <span className={s.status}>
+        <span
+          className={`${s.content} ${statusKey === 'connecting' ? s.pulsing : ''}`}
+          style={{ backgroundColor: color }}
+        />
+        <span>{text}</span>
+      </span>
     </div>
   )
 }
